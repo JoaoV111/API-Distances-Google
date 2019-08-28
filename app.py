fae220966bf209bbf3bffae1faca303532d5adf2
João Vitor <joaocesar93@hotmail.com>
2019-08-28 11:28:24 -0300

    Update

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ from urllib.request import Request, urlopen
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/music-lyrics/')
+def MusicLyrics():
+    request = Request('https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime')
+    response_body = urlopen(request).read()
+    return render_template('musiclyrics.html', response_body=response_body)
+
 if __name__ == '__main__':
     app.run(debug=True)
-	
-@app.route("/music-lyrics/")
-def MusicLyrics():
-	request = Request('https://api.lyrics.ovh/v1/slipknot/before i forget')
-	response_body = urlopen(request).read()
-	return render_template('musiclyrics.html')
+
 
 
 
