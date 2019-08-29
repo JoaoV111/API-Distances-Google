c7120e8f1592c9ae26fd61992f4499d9b7949862
João Vitor <joaocesar93@hotmail.com>
2019-08-28 23:14:07 -0300

    Update music lyrics

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 
@@ -8,13 +8,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/music-lyrics/')
+@app.route('/music-lyrics/', methods=['POST', 'GET'])
 def MusicLyrics():
-	request = requests.get('https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime')
-	lyrics_json = json.loads(request.text)
-	lyrics = lyrics_json["lyrics"]
-	lyrics = lyrics.replace("\n","<br/>")
-	return render_template('musiclyrics.html', lyrics=lyrics)
+	lyrics = ''
+	Artist = 'artist'
+	Title = 'title'
+	if request.method == 'POST':
+		Artist = request.form['artist']
+		Title = request.form['title']
+		if Artist == '' or Title == '':
+			Artist = 'artist'
+			Title = 'title'
+		try:
+			url = f'https://api.lyrics.ovh/v1/{Artist}/{Title}'
+			req = requests.get(url)
+			lyrics_json = json.loads(req.text)
+			lyrics = lyrics_json["lyrics"]
+			lyrics = lyrics.replace("\n","<br>")
+		except:
+			lyrics = 'No lyrics found.'
+	
+	return render_template('musiclyrics.html', lyrics=lyrics, Artist=Artist, Title=Title)
 
 if __name__ == '__main__':
     app.run(debug=True)
