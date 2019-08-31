465c845a19dab0851dcdab823c8bb27ab88fc4b1
João Vitor <joaocesar93@hotmail.com>
2019-08-31 14:13:46 -0300

    Huge Upgrade

--- app.py
+++ app.py
@@ -30,7 +30,31 @@ def MusicLyrics():
 	
 	return render_template('musiclyrics.html', lyrics=lyrics, Artist=Artist, Title=Title)
 
-	
+@app.route('/crypto-currency/', methods=['POST', 'GET'])
+def CryptoCurrency():
+	f_id = ''
+	s_id = ''
+
+	if request.method == 'POST':
+		f_id = resquest.form['f_id']
+		s_id = resquest.form['s_id']
+		
+		try:
+			url = f'https://joao-api-cryptocurrency.herokuapp.com/currency?f_id={f_id}&s_id={s_id}'
+			req = requests.get(url)
+			req_json = json.loads(req.text)
+			result = ''
+			div1 = req_json["div1"]
+		except:
+			result = 'No currencies found.'
+		
+		try:
+			url = 'https://joao-api-cryptocurrency.herokuapp.com/currency/all'
+			req_all = requests.get(url)
+		except:
+			result = 'Server error'
+
+	return render_template('currency.html', result=result, div1=div1, req_all=req_all)
 
 if __name__ == '__main__':
     app.run(debug=True)
