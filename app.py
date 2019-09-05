a4da5efd898ff6e17f0337f040ea4b17eea4f4d3
João Vitor <joaocesar93@hotmail.com>
2019-09-04 22:23:41 -0300

    error correction

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ def MusicLyrics():
 	if request.method == 'POST':
 		Artist = request.form['artist'].strip()
 		Title = request.form['title'].strip()
-		if Artist == '' or Title == '':
-			Artist = 'artist'
-			Title = 'title'
 		try:
 			url = f'https://api.lyrics.ovh/v1/{Artist}/{Title}'
 			req = requests.get(url)
@@ -26,19 +23,15 @@ def MusicLyrics():
 			lyrics = lyrics_json["lyrics"]
 			lyrics = lyrics.replace("\n","<br>")
 		except:
-			lyrics = 'No lyrics found.'
+			lyrics = 'No lyrics found.', 404
 	
 	return render_template('musiclyrics.html', lyrics=lyrics, Artist=Artist, Title=Title)
 
 @app.route('/crypto-currency/', methods=['POST', 'GET'])
 def CryptoCurrency():
-	f_id = ''
-	s_id = ''
-	f_name = ''
-	s_name = ''
-	f_value = 1.0
 	result = ''
 	req_json = [{},{}]
+	f_value = 1.0
 
 	if request.method == 'POST':
 		f_name = request.form['f_name'].strip().capitalize()
@@ -50,21 +43,23 @@ def CryptoCurrency():
 			url = 'https://joao-api-cryptocurrency.herokuapp.com/currency/all'
 			req_all = requests.get(url)
 			req_json_all = json.loads(req_all.text)
+			f_id = ''
+			s_id = ''
 			for currency in req_json_all:
 				if currency['name'].lower() == f_name.lower():
 					f_id = currency['id']
 				if currency['name'].lower() == s_name.lower():
 					s_id = currency['id']
 			url = (f'https://joao-api-cryptocurrency.herokuapp.com/currency'
-				   f'?f_id={f_id}&s_id={s_id}&f_value={f_value}')
+				   f'?f_id={f_id}&s_id={s_id}&f_value={f_value}')	
 			req = requests.get(url)
 			req_json = json.loads(req.text)
-			if req_json == [{}]:
+			if req.status_code != 200:
 				req_json = [{},{}]
-				result = 'Currencies not found.'
+				result = 'Currencies not found.', 404
 		except:
-			result = 'Server Error.'
-		
+			result = 'Server Error.', 500
+	
 	return render_template('currency.html', result=result, req_json=req_json, f_value=f_value)
 
 if __name__ == '__main__':
